ddf5f47119cb6379ea8dbe1a8f57316e4fa1af64
Mauro Sanchez <novamauro98@gmail.com>
2021-09-16 14:24:23 -0500

    Added functionality for when grave yard.

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 let apps = ["app", "blog", "shop", "note", "game", "grave"];
 let appName = document.getElementById("app-name");
 appName.innerHTML = apps[0];
+
+const toggleDark = () => {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+    appName.style.color = "#F6F6F6"
+};
+const toggleLight = () => {
+    document.body.style.color = "black";
+    document.body.style.backgroundColor = "white";
+    appName.style.color = "#F6F6F6"
+};
 const rotateRight = () => {
     apps.push(apps.shift())
     appName.innerHTML = apps[0];
+    apps[0] === "grave" ? toggleDark() : toggleLight();
     return apps;
 };
 const rotateLeft = () => {
     apps.unshift(apps.pop());
     appName.innerHTML = apps[0];
+    apps[0] === "grave" ? toggleDark() : toggleLight();
     return apps;
 };
 document.getElementById("right-button").onclick = rotateRight;
